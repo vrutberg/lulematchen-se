86a2d5571760c691cc5b42f66c16e5f8854962b7
Viktor Rutberg <wishie@gmail.com>
2016-01-21 20:50:21 +0100

    Removd all games route.

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -3,7 +3,5 @@ var appModule = angular.module('app', ['ngRoute']);
 appModule.config(function($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: 'views/next-game/next-game-template.html'
-  }).when('/all-games', {
-    templateUrl: 'views/game-list/game-list-template.html'
   });
 });
