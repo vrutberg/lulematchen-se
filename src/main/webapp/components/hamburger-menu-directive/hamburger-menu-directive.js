d6cb68c5623f04d39113feee611eafb45c813830
Viktor Rutberg <wishie@gmail.com>
2016-02-13 20:58:35 +0100

    Added feature detection of audio autoplay.

--- src/main/webapp/components/hamburger-menu-directive/hamburger-menu-directive.js
+++ src/main/webapp/components/hamburger-menu-directive/hamburger-menu-directive.js
@@ -1,6 +1,16 @@
-angular.module('app').directive('hamburgerMenu', function(ApiService, $interval) {
+angular.module('app').directive('hamburgerMenu', function() {
   return {
     templateUrl: 'components/hamburger-menu-directive/hamburger-menu-template.html',
+
+    scope: {},
+
+    controller: ['$scope', function($scope) {
+      $scope.isAutoplaySupported = false;
+
+      $scope.$watch(function() { return Modernizr.autoplay; }, function(newValue, oldValue) {
+        $scope.isAutoplaySupported = newValue;
+      });
+    }],
 
     link: function() {
       $('body').addClass('drawer drawer--right');
