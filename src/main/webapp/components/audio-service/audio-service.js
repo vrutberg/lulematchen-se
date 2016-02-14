2c9bd7a3a4d6ed5092281b3def8c55033c2fb5c2
Viktor Rutberg <wishie@gmail.com>
2016-02-14 18:38:04 +0100

    Removed check if we should play audio from low-level AudioService.

--- src/main/webapp/components/audio-service/audio-service.js
+++ src/main/webapp/components/audio-service/audio-service.js
@@ -1,13 +1,7 @@
 angular.module('app').factory('AudioService', ['SettingsService', 'Modernizr', function(SettingsService, Modernizr) {
-  var shouldPlayAudio = function() {
-    return SettingsService.isAudioEnabled() && Modernizr.autoplay;
-  };
-
   return {
     playAudio: function(audioFile) {
-      if (shouldPlayAudio()) {
-        new Audio(audioFile).play();
-      }
+      new Audio(audioFile).play();
     }
   };
 }]);
